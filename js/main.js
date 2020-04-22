2aa663890546b49d63cf0b7ee3677c76acc59ad1
Anton <yuschkof.toscha@yandex.ru>
2020-04-22 12:52:09 +0400

    beautiful code

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 window.requestAnimFrame = (function () {
-    return window.requestAnimationFrame       ||
-           window.webkitRequestAnimationFrame ||
-           window.mozRequestAnimationFrame    ||
-           window.oRequestAnimationFrame      ||
-           window.msRequestAnimationFrame     ||
-           function (callbacks) {
-               window.setTimeout(callbacks, 1000 / 60);
-           };
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (callbacks) {
+            window.setTimeout(callbacks, 1000 / 60);
+        };
 })();
 
 window.onload = function () {
@@ -20,11 +20,30 @@ window.onload = function () {
     };
     const ZOOM_OUT = 1.1;
     const ZOOM_IN = 0.9;
-
     const sur = new Surfaces;
-    const canvas = new Canvas({ width: 600, height: 600, WINDOW, callbacks: { wheel, mouseup, mousedown, mousemove, mouseleave}});
-    const graph3D = new Graph3D({ WINDOW });
-    const ui = new UI({callbacks: { move, printPoints, printEdges, printPolygons }});
+    const canvas = new Canvas({
+        width: 600,
+        height: 600,
+        WINDOW,
+        callbacks: {
+            wheel,
+            mouseup,
+            mousedown,
+            mousemove,
+            mouseleave
+        }
+    });
+    const graph3D = new Graph3D({
+        WINDOW
+    });
+    const ui = new UI({
+        callbacks: {
+            move,
+            printPoints,
+            printEdges,
+            printPolygons
+        }
+    });
 
     const SCENE = [sur.bublik()]; // сцена
 
@@ -56,8 +75,8 @@ window.onload = function () {
 
     function mouseup() {
         canRotate = false;
-    } 
-    
+    }
+
     function mouseleave() {
         mouseup();
     }
@@ -95,23 +114,23 @@ window.onload = function () {
             subject.polygons.sort((a, b) => b.distance - a.distance);
             for (let i = 0; i < subject.polygons.length; i++) {
                 const polygon = subject.polygons[i];
-                const point1 = { 
-                    x: graph3D.xs(subject.points[polygon.points[0]]), 
-                    y: graph3D.ys(subject.points[polygon.points[0]]) 
+                const point1 = {
+                    x: graph3D.xs(subject.points[polygon.points[0]]),
+                    y: graph3D.ys(subject.points[polygon.points[0]])
                 };
-                const point2 = { 
-                    x: graph3D.xs(subject.points[polygon.points[1]]), 
-                    y: graph3D.ys(subject.points[polygon.points[1]]) 
+                const point2 = {
+                    x: graph3D.xs(subject.points[polygon.points[1]]),
+                    y: graph3D.ys(subject.points[polygon.points[1]])
                 };
-                const point3 = { 
-                    x: graph3D.xs(subject.points[polygon.points[2]]), 
-                    y: graph3D.ys(subject.points[polygon.points[2]]) 
+                const point3 = {
+                    x: graph3D.xs(subject.points[polygon.points[2]]),
+                    y: graph3D.ys(subject.points[polygon.points[2]])
                 };
-                const point4 = { 
-                    x: graph3D.xs(subject.points[polygon.points[3]]), 
-                    y: graph3D.ys(subject.points[polygon.points[3]]) 
-                }; 
-                canvas.polygon([point1, point2,point3, point4], polygon.color);
+                const point4 = {
+                    x: graph3D.xs(subject.points[polygon.points[3]]),
+                    y: graph3D.ys(subject.points[polygon.points[3]])
+                };
+                canvas.polygon([point1, point2, point3, point4], polygon.color);
             }
         };
 
@@ -121,27 +140,27 @@ window.onload = function () {
                 const point1 = subject.points[edges.p1];
                 const point2 = subject.points[edges.p2];
                 canvas.line(graph3D.xs(point1), graph3D.ys(point1), graph3D.xs(point2), graph3D.ys(point2));
-            } 
+            }
         }
-        
+
         if (canPrint.points) {
             for (let i = 0; i < subject.points.length; i++) {
                 const points = subject.points[i];
                 canvas.point(graph3D.xs(points), graph3D.ys(points));
-            } 
-        }      
+            }
+        }
     }
 
     function render() {
         canvas.clear();
         SCENE.forEach(subject => printSubject(subject));
-        canvas.text(WINDOW.LEFT, WINDOW.BOTTOM,'FPS: ' + FPSout);
-        
+        canvas.text(WINDOW.LEFT, WINDOW.BOTTOM, 'FPS: ' + FPSout);
+
     }
-    
+
     let FPS = 0;
     let FPSout = 0;
-    let timestamp = (new Date()).getTime(); 
+    let timestamp = (new Date()).getTime();
     (function animloop() {
         FPS++;
         const currentTimestamp = (new Date()).getTime();
@@ -154,4 +173,4 @@ window.onload = function () {
         render();
         requestAnimFrame(animloop);
     })();
-}; 
+};
